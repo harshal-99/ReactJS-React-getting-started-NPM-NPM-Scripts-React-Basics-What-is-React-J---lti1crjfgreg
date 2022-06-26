9b13f5c55820606ef1aa5441da885a8aba316f26
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-26 19:30:21 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,47 @@
-import React, {Component, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const prevRef = useRef()
+  const nextRef = useRef()
+  useEffect(() => {
+    prevRef.current.setAttribute('disabled', 'true')
+  }, [])
+  const onNext = (event) => {
+    if (currentIndex >= 0) {
+      prevRef.current.removeAttribute('disabled')
+    }
+    if (currentIndex + 1 >= slides.length) {
+      event.target.setAttribute('disabled', 'true')
+      return
+    }
+    setCurrentIndex(prev => prev + 1)
+  }
+  const onPrev = (event) => {
+    if (currentIndex < slides.length) {
+      nextRef.current.removeAttribute('disabled')
+    }
+    if (currentIndex - 1 <= 0) {
+      event.target.setAttribute('disabled', 'true')
+      return
+    }
+    setCurrentIndex(prev => prev - 1)
+  }
+  const onRestart = (event) => {
+    setCurrentIndex(0)
+    prevRef.current.setAttribute('disabled', 'true')
+  }
   return (
-    <></>
+    <div>
+      <div>
+        <h1 data-test-id="title">{slides[currentIndex].title}</h1>
+        <p data-testid="text">{slides[currentIndex].text}</p>
+      </div>
+      <button data-testid="button-restart" onClick={onRestart}>Restart</button>
+      <button data-testid="button-prev" onClick={onPrev} ref={prevRef}>Prev</button>
+      <button data-testid="button-next" onClick={onNext} ref={nextRef}>Next</button>
+    </div>
   )
 }
 
