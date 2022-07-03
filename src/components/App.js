fdf89a0614f4370690c8cbf5812dc65f940af127
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-07-03 22:28:44 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -13,32 +13,32 @@ const App = ({ slides }) => {
   // }, [])
 
   const onNext = (event) => {
-    restartRef.current.removeAttribute('disabled')
-    if (currentIndex > 0) {
-      prevRef.current.removeAttribute('disabled')
-    }
-    if (currentIndex + 1 >= slides.length) {
-      event.target.setAttribute('disabled', 'true')
-      return
-    }
+    // restartRef.current.removeAttribute('disabled')
+    // if (currentIndex > 0) {
+    //   prevRef.current.removeAttribute('disabled')
+    // }
+    // if (currentIndex + 1 >= slides.length) {
+    //   event.target.setAttribute('disabled', 'true')
+    //   return
+    // }
     setCurrentIndex(currentIndex + 1)
   }
   const onPrev = (event) => {
-    if (currentIndex < slides.length) {
-      nextRef.current.removeAttribute('disabled')
-    }
-    if (currentIndex === 0) {
-      console.log(currentIndex);
-      // restartRef.current.removeAttribute('disabled')
-      event.target.setAttribute('disabled', 'true')
-      return
-    }
+    // if (currentIndex < slides.length) {
+    //   nextRef.current.removeAttribute('disabled')
+    // }
+    // if (currentIndex === 0) {
+    //   console.log(currentIndex);
+    //   // restartRef.current.removeAttribute('disabled')
+    //   event.target.setAttribute('disabled', 'true')
+    //   return
+    // }
     setCurrentIndex(currentIndex - 1)
   }
   const onRestart = (event) => {
     setCurrentIndex(0)
-    restartRef.current.setAttribute('disabled', 'true')
-    prevRef.current.setAttribute('disabled', 'true')
+    // restartRef.current.setAttribute('disabled', 'true')
+    // prevRef.current.setAttribute('disabled', 'true')
   }
   return (
     <div>
@@ -46,9 +46,9 @@ const App = ({ slides }) => {
         <h1 data-testid="title">{slides[currentIndex].title}</h1>
         <p data-testid="text">{slides[currentIndex].text}</p>
       </div>
-      <button data-testid="button-restart" onClick={onRestart} ref={restartRef}>Restart</button>
-      <button data-testid="button-prev" onClick={onPrev} ref={prevRef}>Prev</button>
-      <button data-testid="button-next" onClick={onNext} ref={nextRef}>Next</button>
+      <button data-testid="button-restart" disabled={currentIndex === 0} onClick={onRestart} ref={restartRef}>Restart</button>
+      <button data-testid="button-prev" disabled={currentIndex === 0} onClick={onPrev} ref={prevRef}>Prev</button>
+      <button data-testid="button-next" disabled={currentIndex === slides.length - 1} onClick={onNext} ref={nextRef}>Next</button>
     </div>
   )
 }
